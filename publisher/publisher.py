3471b7773eeeb1620b809d13abea5b8073df83bc
Rodrigo Mariano <rodmariano13@gmail.com>
2021-02-08 12:26:19 -0300

    refactor: add SatelliteMetadata class

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -17,26 +17,13 @@ from publisher.validator import validate, QUERY_SCHEMA
 logger = create_logger(__name__, level=PR_LOGGING_LEVEL)
 
 
-class Publisher:
-    def __init__(self, BASE_DIR, IS_TO_GET_DATA_FROM_DB, db_connection, query=None):
-        # base directory to search the files
-        self.BASE_DIR = BASE_DIR
-        self.IS_TO_GET_DATA_FROM_DB = IS_TO_GET_DATA_FROM_DB
+class SatelliteMetadata:
+
+    def __init__(self):
         self.SATELLITES = None
-        self.db = db_connection
-        self.errors = []
-        self.query = query
 
         # read satellites metadata file
         self.__read_metadata_file()
-
-        if self.IS_TO_GET_DATA_FROM_DB:
-            # get all available collections from database and save the result in a CSV file
-            self.df_collections = self.db.select_from_collections()
-            self.df_collections.to_csv(f'{PR_FILES_PATH}/collections.csv', index=False)
-        else:
-            # get all available collections from CSV file
-            self.df_collections = read_csv(f'{PR_FILES_PATH}/collections.csv')
 
     def __read_metadata_file(self):
         '''Read JSON satellite metadata file.'''
@@ -48,7 +35,7 @@ class Publisher:
             # read JSON file and convert it to dict
             self.SATELLITES = loads(data.read())
 
-    def __get_assets_metadata(self, satellite=None, sensor=None, radio_processing=None, **kwargs):
+    def get_assets_metadata(self, satellite=None, sensor=None, radio_processing=None, **kwargs):
         '''Get assets metadata based on the parameters.'''
 
         # get the satellite information
@@ -69,6 +56,26 @@ class Publisher:
         # if a sensor has been found, then get the unique value inside the list
         # and return assets metadata based on the radiometric processing (i.e. DN or SR)
         return  sensor[0]['assets'][radio_processing]
+
+
+class Publisher:
+
+    def __init__(self, BASE_DIR, IS_TO_GET_DATA_FROM_DB, db_connection, query=None):
+        # base directory to search the files
+        self.BASE_DIR = BASE_DIR
+        self.IS_TO_GET_DATA_FROM_DB = IS_TO_GET_DATA_FROM_DB
+        self.satellite_metadata = SatelliteMetadata()
+        self.db = db_connection
+        self.errors = []
+        self.query = query
+
+        if self.IS_TO_GET_DATA_FROM_DB:
+            # get all available collections from database and save the result in a CSV file
+            self.df_collections = self.db.select_from_collections()
+            self.df_collections.to_csv(f'{PR_FILES_PATH}/collections.csv', index=False)
+        else:
+            # get all available collections from CSV file
+            self.df_collections = read_csv(f'{PR_FILES_PATH}/collections.csv')
 
     def __get_dn_xml_file(self, files, dir_path):
         '''Return just one DN XML file inside the directory as a dictionary.'''
@@ -138,7 +145,7 @@ class Publisher:
             print_line()
             logger.info(f'item: {item}\n')
 
-            assets_metadata = self.__get_assets_metadata(**item['collection'])
+            assets_metadata = self.satellite_metadata.get_assets_metadata(**item['collection'])
             logger.info(f'assets_metadata: {assets_metadata}\n')
 
             item['assets'] = create_assets_from_metadata(assets_metadata, dir_path)

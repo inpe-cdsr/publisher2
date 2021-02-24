46dfd35b7b4a91246cd26629c1f62a084ef70b9a
Rodrigo Mariano <rodmariano13@gmail.com>
2021-02-24 09:54:58 -0300

    feat: add environment variable to control task chunks

--- publisher/environment.py
+++ publisher/environment.py
@@ -9,22 +9,33 @@ def str2bool(value):
     return str(value).lower() in ('true', 't', '1', 'yes', 'y')
 
 
+##################################################
+# Flask environment variables
+##################################################
+
 FLASK_SECRET_KEY = getenv('FLASK_SECRET_KEY', 'test')
 
 FLASK_TESTING = str2bool(getenv('FLASK_TESTING', 'False'))
 
-# base directory to recursively traverse
-PR_BASE_DIR = getenv('PR_BASE_DIR', '/')
+
+##################################################
+# Publisher environment variables
+##################################################
+
+# base directory to recursively traverse, base directory to search assets
+PR_BASE_DIR = getenv('PR_BASE_DIR', '/TIFF')
 # path to files folder
 PR_FILES_PATH = getenv('PR_FILES_PATH', 'files')
 
 PR_IS_TO_GET_DATA_FROM_DB = str2bool(getenv('PR_IS_TO_GET_DATA_FROM_DB', 'True'))
 
 PR_LOGGING_LEVEL = getenv('PR_LOGGING_LEVEL', 'INFO')
+# number of chunks per celery task
+PR_TASK_CHUNKS = getenv('PR_TASK_CHUNKS', 10)
 
 # if the inserted logging level already exists, then select it,
-# else, insert a default logging level
 if PR_LOGGING_LEVEL in AVAILABLE_LEVELS:
     PR_LOGGING_LEVEL = AVAILABLE_LEVELS[PR_LOGGING_LEVEL]
 else:
+    # else, insert a default logging level
     PR_LOGGING_LEVEL = INFO

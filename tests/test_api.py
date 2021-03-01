e3df81e40b0f6f6880bded085ceefa589005e9ea
Rodrigo Mariano <rodmariano13@gmail.com>
2021-03-01 08:38:13 -0300

    test: refactor async test cases

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,7 +18,7 @@ db_publisher = PostgreSQLPublisherConnection()
 celery_async = ('publisher.workers.celery_config.task_always_eager', False)
 celery_sync = ('publisher.workers.celery_config.task_always_eager', True)
 
-test_delay_secs = 3
+test_delay_secs = 0.6
 
 
 class BaseTestCases:
@@ -67,31 +67,28 @@ class BaseTestCases:
             result = db_publisher.select_from_task_error()
             self.assertEqual(expected, result)
 
-'''
+
 @mock.patch(*celery_async)
 class AsyncApiPublishOkTestCase(BaseTestCases.BaseTestCase):
 
-    def test__async__api_publish__ok__empty_query(self):
-        self.maxDiff=None
+    def __execute_publisher(self, query):
+        self.get(query_string=query)
+
+        # wait N seconds to the task save the data in the database
+        # before checking if the data has been inserted correctly
+        sleep(test_delay_secs)
+
+    # CBERS2B
+
+    def test__api_publish__ok__cbers2b(self):
+        query = {
+            'satellite': 'CBERS2B'
+        }
+
         expected = [
             {
                 'message': 'There is metadata to the `CBERS2B_XYZ_L2_DN` collection, however this collection does not exist in the database.',
                 'metadata': {'collection': 'CBERS2B_XYZ_L2_DN'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2016_01/CBERS_4_MUX_DRD_2016_01_01.13_28_32_CB11/157_101_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_07/CBERS_4_MUX_DRD_2020_07_31.13_07_00_CB11/155_103_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84'},
                 'type': 'warning'
             },
             {
@@ -108,60 +105,53 @@ class AsyncApiPublishOkTestCase(BaseTestCases.BaseTestCase):
                 'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
                 'metadata': {'folder': '/TIFF/CBERS2B/2007_09/CBERS2B_WFI_20070928.131338/154_124_0/2_BC_LCC_WGS84'},
                 'type': 'warning'
-            },
+            }
+        ]
+
+        self.__execute_publisher(query)
+
+        self.check_if_the_items_have_been_added_in_the_database(
+            'cbers2b/test__api_publish__ok__cbers2b.csv'
+        )
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    def test__api_publish__ok__cbers2b_ccd_l2_dn__quicklook_does_not_exist(self):
+        # CBERS2B/2007_09/CBERS2B_CCD_20070925.145654/181_096_0/2_BC_UTM_WGS84
+        query = {
+            'satellite': 'CBeRS2B',
+            'sensor': 'CCd',
+            'start_date': '2007-09-25',
+            'end_date': '2007-09-25',
+            'path': 181,
+            'row': '096',
+            'geo_processing': 2,
+            'radio_processing': 'DN'
+        }
+
+        expected = [{
+            'type': 'warning',
+            'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+            'metadata': {
+                'folder': '/TIFF/CBERS2B/2007_09/CBERS2B_CCD_20070925.145654/181_096_0/2_BC_UTM_WGS84'
+            }
+        }]
+
+        self.__execute_publisher(query)
+
+        self.check_if_the_items_table_is_empty()
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    # CBERS4A
+
+    def test__api_publish__ok__cbers4a(self):
+        query = {
+            'satellite': 'CBERS4A'
+        }
+
+        expected = [
             {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT1/1976_10/LANDSAT1_MSS_19761002.120000/010_057_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT2/1975_07/LANDSAT2_MSS_19750724.123000/230_070_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT3/1982_08/LANDSAT3_MSS_19820802.120000/231_072_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT5/1984_04/LANDSAT5_TM_19840406.124930/223_062_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT7/1999_07/LANDSAT7_ETM_19990719.124008/217_064_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT7/2003_06/LANDSAT7_ETM_20030601.125322/220_061_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `BAND13.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_135_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `BAND13.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_136_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `BAND13.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_137_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `CMASK_GRID_SURFACE.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_137_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'This folder is valid, but it is empty.',
-                'metadata': {'folder': '/TIFF/CBERS4/2021_02/CBERS_4_PAN10M_DRD_2021_02_02.01_32_45_CB11/073_113_0/4_BC_UTM_WGS84'},
+                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84'},
                 'type': 'warning'
             },
             {
@@ -211,20 +201,92 @@ class AsyncApiPublishOkTestCase(BaseTestCases.BaseTestCase):
             }
         ]
 
-        self.get()
-
-        # wait N seconds to the task save the data in the database
-        # before check if the data has been inserted correctly
-        sleep(test_delay_secs)
+        self.__execute_publisher(query)
 
         self.check_if_the_items_have_been_added_in_the_database(
-            'test__api_publish__ok__empty_query.csv'
+            'cbers4a/test__api_publish__ok__cbers4a.csv'
         )
         self.check_if_the_errors_have_been_added_in_the_database(expected)
-'''
+
+    def test__api_publish__ok__cbers4a_mux_l4_dn_or_sr__dn_file_does_not_exist(self):
+        # CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84
+        query = {
+            'satellite': 'cBERs4A',
+            'sensor': 'mux',
+            'start_date': '2019-12-28',
+            'end_date': '2019-12-28',
+            'path': 221,
+            'row': 108,
+            'geo_processing': 4
+        }
+
+        expected = [{
+            'type': 'warning',
+            'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
+            'metadata': {
+                'folder': '/TIFF/CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84'
+            }
+        }]
+
+        self.__execute_publisher(query)
+
+        self.check_if_the_items_table_is_empty()
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    # LANDSAT1
+
+    def test__api_publish__ok__landsat1(self):
+        query = {
+            'satellite': 'LANDSAT1'
+        }
+
+        expected = [
+            {
+                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/LANDSAT1/1976_10/LANDSAT1_MSS_19761002.120000/010_057_0/2_BC_UTM_WGS84'},
+                'type': 'warning'
+            }
+        ]
+
+        self.__execute_publisher(query)
+
+        self.check_if_the_items_have_been_added_in_the_database(
+            'landsat/test__api_publish__ok__landsat1.csv'
+        )
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    # LANDSAT7
+
+    def test__api_publish__ok__landsat7(self):
+        query = {
+            'satellite': 'LANDSAT7'
+        }
+
+        expected = [
+            {
+                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/LANDSAT7/1999_07/LANDSAT7_ETM_19990719.124008/217_064_0/2_BC_UTM_WGS84'},
+                'type': 'warning'
+            },
+            {
+                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/LANDSAT7/2003_06/LANDSAT7_ETM_20030601.125322/220_061_0/2_BC_UTM_WGS84'},
+                'type': 'warning'
+            }
+        ]
+
+        self.__execute_publisher(query)
+
+        self.check_if_the_items_have_been_added_in_the_database(
+            'landsat/test__api_publish__ok__landsat7.csv'
+        )
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
 
 @mock.patch(*celery_sync)
 class ApiPublishCbers2BOkTestCase(BaseTestCases.BaseTestCase):
+
+    # CBERS2B
 
     def test__api_publish__ok__cbers2b(self):
         self.maxDiff=None
@@ -804,6 +866,8 @@ class ApiPublishCbers4OkTestCase(BaseTestCases.BaseTestCase):
 
 @mock.patch(*celery_sync)
 class ApiPublishCbers4AOkTestCase(BaseTestCases.BaseTestCase):
+
+    # CBERS4A
 
     def test__api_publish__ok__cbers4a(self):
         self.maxDiff=None
--- tests/test_publisher.py
+++ tests/test_publisher.py
@@ -4,12 +4,9 @@ from unittest import mock, TestCase
 from pandas import read_csv, to_datetime
 from pandas.testing import assert_frame_equal
 
-from publisher.environment import PR_BASE_DIR, PR_FILES_PATH, PR_TASK_CHUNKS
+from publisher import Publisher
+from publisher.environment import PR_BASE_DIR
 from publisher.model import PostgreSQLCatalogTestConnection, PostgreSQLPublisherConnection
-from publisher.publisher import generate_chunk_params, SatelliteMetadata
-from publisher.util import PublisherWalk
-from publisher.validator import validate, QUERY_SCHEMA
-from publisher.workers import CELERY_TASK_QUEUE, process_items
 
 
 db = PostgreSQLCatalogTestConnection()
@@ -17,7 +14,7 @@ db_publisher = PostgreSQLPublisherConnection()
 
 celery_async = ('publisher.workers.celery_config.task_always_eager', False)
 
-test_delay_secs = 3
+test_delay_secs = 0.6
 
 
 class BaseTestCases:
@@ -55,32 +52,31 @@ class BaseTestCases:
             result = db_publisher.select_from_task_error()
             self.assertEqual(expected, result)
 
-'''
+
 @mock.patch(*celery_async)
 class AsyncPublisherOkTestCase(BaseTestCases.BaseTestCase):
 
-    def test__async__publisher__ok__empty_query(self):
-        self.maxDiff=None
+    @staticmethod
+    def __create_and_execute_publisher(query):
+        # create Publisher object and run the main method
+        publisher_app = Publisher(PR_BASE_DIR, False, None, query=query)
+        publisher_app.main()
+
+        # wait N seconds to the task save the data in the database
+        # before checking if the data has been inserted correctly
+        sleep(test_delay_secs)
+
+    # CBERS2B
+
+    def test__publisher__ok__cbers2b(self):
+        query = {
+            'satellite': 'CBERS2B'
+        }
 
         expected = [
             {
                 'message': 'There is metadata to the `CBERS2B_XYZ_L2_DN` collection, however this collection does not exist in the database.',
                 'metadata': {'collection': 'CBERS2B_XYZ_L2_DN'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2016_01/CBERS_4_MUX_DRD_2016_01_01.13_28_32_CB11/157_101_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_07/CBERS_4_MUX_DRD_2020_07_31.13_07_00_CB11/155_103_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84'},
                 'type': 'warning'
             },
             {
@@ -97,60 +93,53 @@ class AsyncPublisherOkTestCase(BaseTestCases.BaseTestCase):
                 'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
                 'metadata': {'folder': '/TIFF/CBERS2B/2007_09/CBERS2B_WFI_20070928.131338/154_124_0/2_BC_LCC_WGS84'},
                 'type': 'warning'
-            },
+            }
+        ]
+
+        AsyncPublisherOkTestCase.__create_and_execute_publisher(query)
+
+        self.check_if_the_items_have_been_added_in_the_database(
+            'cbers2b/test__api_publish__ok__cbers2b.csv'
+        )
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    def test__publisher__ok__cbers2b_ccd_l2_dn__quicklook_does_not_exist(self):
+        # CBERS2B/2007_09/CBERS2B_CCD_20070925.145654/181_096_0/2_BC_UTM_WGS84
+        query = {
+            'satellite': 'CBeRS2B',
+            'sensor': 'CCd',
+            'start_date': '2007-09-25',
+            'end_date': '2007-09-25',
+            'path': 181,
+            'row': '096',
+            'geo_processing': 2,
+            'radio_processing': 'DN'
+        }
+
+        expected = [{
+            'type': 'warning',
+            'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+            'metadata': {
+                'folder': '/TIFF/CBERS2B/2007_09/CBERS2B_CCD_20070925.145654/181_096_0/2_BC_UTM_WGS84'
+            }
+        }]
+
+        AsyncPublisherOkTestCase.__create_and_execute_publisher(query)
+
+        self.check_if_the_items_table_is_empty()
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    # CBERS4A
+
+    def test__publisher__ok__cbers4a(self):
+        query = {
+            'satellite': 'CBERS4A'
+        }
+
+        expected = [
             {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT1/1976_10/LANDSAT1_MSS_19761002.120000/010_057_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT2/1975_07/LANDSAT2_MSS_19750724.123000/230_070_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT3/1982_08/LANDSAT3_MSS_19820802.120000/231_072_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT5/1984_04/LANDSAT5_TM_19840406.124930/223_062_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT7/1999_07/LANDSAT7_ETM_19990719.124008/217_064_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/LANDSAT7/2003_06/LANDSAT7_ETM_20030601.125322/220_061_0/2_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `BAND13.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_135_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `BAND13.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_136_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `BAND13.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_137_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'There is NOT a TIFF file in this folder that ends with the `CMASK_GRID_SURFACE.tif` template, then it will be ignored.',
-                'metadata': {'folder': '/TIFF/CBERS4/2020_12/CBERS_4_AWFI_DRD_2020_12_28.13_17_30_CB11/157_137_0/4_BC_UTM_WGS84'},
-                'type': 'warning'
-            },
-            {
-                'message': 'This folder is valid, but it is empty.',
-                'metadata': {'folder': '/TIFF/CBERS4/2021_02/CBERS_4_PAN10M_DRD_2021_02_02.01_32_45_CB11/073_113_0/4_BC_UTM_WGS84'},
+                'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84'},
                 'type': 'warning'
             },
             {
@@ -200,28 +189,84 @@ class AsyncPublisherOkTestCase(BaseTestCases.BaseTestCase):
             }
         ]
 
-        _, query, _ = validate({}, QUERY_SCHEMA)
-
-        # p_walk is a generator that returns just valid directories
-        p_walk = PublisherWalk(PR_BASE_DIR, query, SatelliteMetadata())
-
-        # get all available collections from CSV file
-        df_collections = read_csv(f'{PR_FILES_PATH}/collections.csv')
-
-        # run the tasks by chunks. PR_TASK_CHUNKS chunks are sent to one task
-        tasks = process_items.chunks(
-            generate_chunk_params(p_walk, df_collections), PR_TASK_CHUNKS
-        ).apply_async(queue=CELERY_TASK_QUEUE)
-
-        # wait N seconds to the task save the data in the database
-        # before check if the data has been inserted correctly
-        sleep(test_delay_secs)
-
-        # save the errors in the database
-        p_walk.save_the_errors_in_the_database()
+        AsyncPublisherOkTestCase.__create_and_execute_publisher(query)
 
         self.check_if_the_items_have_been_added_in_the_database(
-            'test__api_publish__ok__empty_query.csv'
+            'cbers4a/test__api_publish__ok__cbers4a.csv'
         )
         self.check_if_the_errors_have_been_added_in_the_database(expected)
-'''
+
+    def test__publisher__ok__cbers4a_mux_l4_dn_or_sr__dn_file_does_not_exist(self):
+        # CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84
+        query = {
+            'satellite': 'cBERs4A',
+            'sensor': 'mux',
+            'start_date': '2019-12-28',
+            'end_date': '2019-12-28',
+            'path': 221,
+            'row': 108,
+            'geo_processing': 4
+        }
+
+        expected = [{
+            'type': 'warning',
+            'message': 'There is NOT a DN XML file in this folder, then it will be ignored.',
+            'metadata': {
+                'folder': '/TIFF/CBERS4A/2019_12/CBERS_4A_MUX_RAW_2019_12_28.14_15_00/221_108_0/4_BC_UTM_WGS84'
+            }
+        }]
+
+        AsyncPublisherOkTestCase.__create_and_execute_publisher(query)
+
+        self.check_if_the_items_table_is_empty()
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    # LANDSAT1
+
+    def test__publisher__ok__landsat1(self):
+        query = {
+            'satellite': 'LANDSAT1'
+        }
+
+        expected = [
+            {
+                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/LANDSAT1/1976_10/LANDSAT1_MSS_19761002.120000/010_057_0/2_BC_UTM_WGS84'},
+                'type': 'warning'
+            }
+        ]
+
+        AsyncPublisherOkTestCase.__create_and_execute_publisher(query)
+
+        self.check_if_the_items_have_been_added_in_the_database(
+            'landsat/test__api_publish__ok__landsat1.csv'
+        )
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+
+    # LANDSAT7
+
+    def test__publisher__ok__landsat7(self):
+        query = {
+            'satellite': 'LANDSAT7'
+        }
+
+        expected = [
+            {
+                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/LANDSAT7/1999_07/LANDSAT7_ETM_19990719.124008/217_064_0/2_BC_UTM_WGS84'},
+                'type': 'warning'
+            },
+            {
+                'message': 'There is NOT a quicklook in this folder, then it will be ignored.',
+                'metadata': {'folder': '/TIFF/LANDSAT7/2003_06/LANDSAT7_ETM_20030601.125322/220_061_0/2_BC_UTM_WGS84'},
+                'type': 'warning'
+            }
+        ]
+
+        AsyncPublisherOkTestCase.__create_and_execute_publisher(query)
+
+        self.check_if_the_items_have_been_added_in_the_database(
+            'landsat/test__api_publish__ok__landsat7.csv'
+        )
+        self.check_if_the_errors_have_been_added_in_the_database(expected)
+

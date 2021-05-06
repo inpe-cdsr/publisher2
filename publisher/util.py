2e366ec1db4b9101e455ea678c45fee20a0c19cc
Rodrigo Mariano <rodmariano13@gmail.com>
2021-05-06 10:44:51 -0300

    refactor: remove old decode functions and use cdsr_pack package

--- publisher/util.py
+++ publisher/util.py
@@ -10,9 +10,11 @@ from re import search
 from shapely.geometry import mapping
 from xmltodict import parse as xmltodict_parse
 
+from cdsr_pack import decode_path, CDSRDecoderException
+from cdsr_pack.decoder import decode_path_row_dir, decode_scene_dir
+
 from publisher.common import fill_string_with_left_zeros, print_line
 from publisher.environment import PR_LOGGING_LEVEL
-from publisher.exception import PublisherDecodeException
 from publisher.logger import create_logger
 from publisher.model import PostgreSQLCatalogTestConnection, PostgreSQLPublisherConnection
 from publisher.utils.geom import raster_convexhull, raster_extent
@@ -351,84 +353,6 @@ def create_item_and_get_insert_clauses(dir_path, metadata, assets, df_collection
 # Generator
 ##################################################
 
-
-def decode_scene_dir(scene_dir):
-    '''Decode a scene directory, returning its information.'''
-
-    scene_dir_first, scene_dir_second = scene_dir.split('.')
-
-    if scene_dir_first.startswith('AMAZONIA_1') or scene_dir_first.startswith('CBERS_4'):
-        # examples:
-        # - AMAZONIA_1_WFI_DRD_2021_03_03.12_57_40_CB11
-        # - AMAZONIA_1_WFI_DRD_2021_03_03.14_35_23_CB11_SIR18
-        # - CBERS_4_MUX_DRD_2020_07_31.13_07_00_CB11
-        # - CBERS_4A_MUX_RAW_2019_12_27.13_53_00_ETC2
-        # - CBERS_4A_MUX_RAW_2019_12_28.14_15_00
-
-        satellite, number, sensor, _, *date = scene_dir_first.split('_')
-        # create satellite name with its number
-        satellite = satellite + number
-        date = '-'.join(date)
-        time = scene_dir_second.split('_')
-
-        if len(time) >= 3:
-            # get the time part from the list of parts
-            # `time` can be: `13_53_00`, `13_53_00_ETC2`, `14_35_23_CB11_SIR18`, etc.
-            time = ':'.join(time[0:3])
-        else:
-            raise PublisherDecodeException()
-
-    elif scene_dir_first.startswith('CBERS2B') or scene_dir_first.startswith('LANDSAT'):
-        # examples: CBERS2B_CCD_20070925.145654
-        # or LANDSAT1_MSS_19750907.130000
-
-        satellite, sensor, date = scene_dir_first.split('_')
-        time = scene_dir_second
-
-        if len(date) != 8:
-            # example: a time should be something like this: '20070925'
-            raise PublisherDecodeException()
-
-        # I build the date string based on the old one (e.g. from '20070925' to '2007-09-25')
-        date = f'{date[0:4]}-{date[4:6]}-{date[6:8]}'
-
-        if len(time) != 6:
-            # example: a time should be something like this: '145654'
-            raise PublisherDecodeException()
-
-        # I build the time string based on the old one (e.g. from '145654' to '14:56:54')
-        time = f'{time[0:2]}:{time[2:4]}:{time[4:6]}'
-
-    else:
-        raise PublisherDecodeException(f'Scene directory cannot be decoded: `{scene_dir}`.')
-
-    return satellite, sensor, date, time
-
-
-def decode_path_row_dir(path_row_dir):
-    splitted_path_row = path_row_dir.split('_')
-
-    if len(splitted_path_row) == 3:
-        # example: `151_098_0`
-        path, row, _ = splitted_path_row
-    elif len(splitted_path_row) == 5:
-        # example: `151_B_141_5_0`
-        path, _, row, *_ = splitted_path_row
-    else:
-        raise PublisherDecodeException(f'Path/row directory cannot be decoded: `{path_row_dir}`.')
-
-    return path, row
-
-
-def decode_geo_processing_dir(geo_processing_dir):
-    geo_processing = geo_processing_dir.split('_')[0]
-
-    if geo_processing == '2' or geo_processing == '2B' or geo_processing == '3' or geo_processing == '4':
-        return geo_processing
-
-    raise PublisherDecodeException(f'Geo. processing directory cannot be decoded: `{geo_processing_dir}`.')
-
-
 def is_there_sr_files_in_the_list_of_files(files):
     # example: CBERS_4_AWFI_20201228_157_135_L4_BAND16_GRID_SURFACE.xml
     sr_template = '^[a-zA-Z0-9_]+BAND\d+_GRID_SURFACE.xml$'
@@ -452,29 +376,6 @@ class PublisherWalk:
 
         # create an iterator from generator method
         self.__generator_iterator = self.__generator()
-
-    def __get_metadata_from_splitted_dir_path(self, splitted_dir_path):
-        # add the metadata based on the directory decode
-        metadata = {}
-
-        # extract `satellite` and `sensor` data from path
-        _, metadata['satellite'], _, scene_dir, path_row_dir, geo_processing_dir = splitted_dir_path
-
-        try:
-            _, metadata['sensor'], *_ = decode_scene_dir(scene_dir)
-            metadata['path'], metadata['row'] = decode_path_row_dir(path_row_dir)
-            metadata['geo_processing'] = decode_geo_processing_dir(geo_processing_dir)
-        except PublisherDecodeException as error:
-            self.errors_insert.append(
-                PostgreSQLPublisherConnection.create_task_error_insert_clause({
-                    'message': error,
-                    'metadata': {'folder': dir_path, 'method': '__get_metadata_from_splitted_dir_path'},
-                    'type': 'error'
-                })
-            )
-            return None
-
-        return metadata
 
     def __create_assets_from_metadata(self, assets_matadata, dir_path):
         '''Create assets object based on assets metadata.'''
@@ -617,7 +518,7 @@ class PublisherWalk:
             def check_scene_dir(scene_dir):
                 try:
                     _, sensor_dir, date_dir, time_dir = decode_scene_dir(scene_dir)
-                except PublisherDecodeException as error:
+                except CDSRDecoderException as error:
                     self.errors_insert.append(
                         PostgreSQLPublisherConnection.create_task_error_insert_clause({
                             'message': error,
@@ -655,7 +556,7 @@ class PublisherWalk:
             def check_path_row_dir(path_row_dir):
                 try:
                     path, row = decode_path_row_dir(path_row_dir)
-                except PublisherDecodeException as error:
+                except CDSRDecoderException as error:
                     self.errors_insert.append(
                         PostgreSQLPublisherConnection.create_task_error_insert_clause({
                             'message': error,
@@ -751,7 +652,7 @@ class PublisherWalk:
                 continue
 
             # if there are not enough metadata, then ignore this folder
-            metadata = self.__get_metadata_from_splitted_dir_path(splitted_dir_path)
+            metadata = decode_path(dir_path)
             if not metadata:
                 continue
 
